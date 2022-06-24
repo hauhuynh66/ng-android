37440d95b5737dfe446736541a3372d70a93f287
hauhuynh66 <hauhuynh66@gmail.com>
2022-06-24 17:41:37 +0700

    update

--- app/src/main/java/com/app/adapter/ExpandableCardAdapter.kt
+++ app/src/main/java/com/app/adapter/ExpandableCardAdapter.kt
@@ -9,19 +9,24 @@ import androidx.recyclerview.widget.RecyclerView
 import com.app.ngn.R
 
 class ExpandableCardAdapter(val context: Activity, val data : ArrayList<Any>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val expandPos: Int = -1
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return CardViewHolder((context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
             .inflate(R.layout.com_search_card, parent, false))
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val expandable = position==0
-        
+        val isExpanded = position == expandPos
+
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return 1
     }
 
-    class CardViewHolder(private val v : View) : RecyclerView.ViewHolder(v)
+    class CardViewHolder(private val v : View) : RecyclerView.ViewHolder(v){
+        fun bind(){
+
+        }
+    }
 }
